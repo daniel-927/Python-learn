464ee01b69fb181bd0c2bdf89d69cccd651bc7b4
daniel <daniel.wang927@gmail.com>
2023-12-12 19:47:13 +0800

    fix

--- main/python_speed_test.py
+++ main/python_speed_test.py
@@ -1,8 +1,8 @@
 import timeit
 # 使用前 先进行编译 python setup.py build_ext --inplace
-python = timeit.timeit('program2.fib(1000000)',
+python = timeit.timeit('program2.fib(100000)',
                        setup='import program2', number=1)
-cython = timeit.timeit('program1.fib(1000000)',
+cython = timeit.timeit('program1.fib(100000)',
                        setup='import program1', number=1)
 
 print("Python Time: ", python)
