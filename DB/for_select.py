a9d6c2c64007dce7167a009151bcf48de040a585
daniel <daniel.wang927@gmail.com>
2023-12-18 13:13:18 +0800

    fix

---
+++ DB/for_select.py
@@ -0,0 +1,56 @@
+import random
+import time
+import pysnooper
+import pymysql
+import pymysql.cursors
+
+
+# 装饰器 计算时间
+def timer(func):
+    def decor(*args):
+        start_time = time.time()
+        func(*args)
+        end_time = time.time()
+        d_time = end_time - start_time
+        print("the running time is : ", d_time)
+
+    return decor
+
+
+db_config = {
+    "host": "52.35.86.227",
+    "port": 9030,
+    "user": "root",
+    "password": "sr@123",
+    "database": "filbet_dev_sharding"
+}
+
+
+@timer
+@pysnooper.snoop()
+def add_test_data():
+    # 打开连接
+    connection = pymysql.connect(**db_config)
+    cursor = connection.cursor()
+
+    # 开始循环查询
+    # results = 0
+    for i in range(1024):
+        # sql = f"ALTER TABLE win_coin_log_{i} ADD COLUMN `merchant_id` int NOT NULL default 0 COMMENT '商户id' AFTER username;"
+        # sql = f"ALTER TABLE win_betslips_{i} ADD COLUMN `merchant_id` int NOT NULL default 0 COMMENT '商户id' AFTER xb_username;"
+        # sql = f"truncate win_betslips_details_{i}"
+        sql = f"show databases;"
+        try:
+            cursor.execute(sql)
+            connection.commit()
+            # result = cursor.fetchmany(1)
+            # result = int(results[0][0])
+            # results += result
+            # print(results)
+        except Exception as e:
+            print(e)
+    # print(results)
+    connection.close()
+
+
+add_test_data()
---
+++ DB/while_select.py
@@ -0,0 +1,61 @@
+# author:  Daniel
+# date :  2023.8.18
+import random
+import time
+import pysnooper
+import pymysql
+import pymysql.cursors
+
+
+# 装饰器 计算时间
+def timer(func):
+    def decor(*args):
+        start_time = time.time()
+        func(*args)
+        end_time = time.time()
+        d_time = end_time - start_time
+        print("the running time is : ", d_time)
+
+    return decor
+
+
+db_config = {
+    "host": "filbet-zi-dev-aurora-cluster.cluster-c0mmrepgi1ky.us-west-2.rds.amazonaws.com",
+    "port": 3306,
+    "user": "admin",
+    "password": "WATNfBJYaZ4FPVVzdYCq",
+    "database": "filbet"
+}
+
+
+@timer
+@pysnooper.snoop()
+def add_test_data():
+    # 打开连接
+    connection = pymysql.connect(**db_config)
+    cursor = connection.cursor()
+
+    # 获取总行数
+    totalrows_sql = f'SELECT COUNT(1)  FROM 0_win_betslips WHERE game_list_id = 0;'
+    cursor.execute(totalrows_sql)
+    totalrows = cursor.fetchmany(1)
+    totalrows = int(totalrows[0][0])
+
+    # 开始循环查询
+    count1 = 1
+    while totalrows > 0:
+        try:
+            temprows = 20000
+            #sql = f'UPDATE 0_win_betslips a	JOIN (SELECT id FROM 0_win_betslips WHERE game_list_id = 0 LIMIT 20000) c ON a.id = c.id LEFT JOIN game_list b ON a.game_type_id = b.code AND a.game_plat_id = b.game_provider_id AND a.game_id = b.game_provider_subtype_id  SET a.game_list_id = b.id, a.game_provider_id = b.game_provider_id, a.game_pagcor_id = b.game_pagcor_id, a.game_provider_subtype_id = b.game_provider_subtype_id,a.game_group_id=b.game_type_id  WHERE a.game_list_id=0; '
+            sql = f"show databases;"
+            cursor.execute(sql)
+            totalrows = totalrows - temprows
+            connection.commit()
+
+        except Exception as e:
+            print(e)
+        count1 += 1
+    connection.close()
+
+
+add_test_data()
