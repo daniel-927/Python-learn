1620606b35ab73673a1d5b41243a74ee06752e5d
Daniel <32482258+daniel-927@users.noreply.github.com>
2024-08-07 20:30:51 +0900

    Update add_partition.py

--- DB/add_partition.py
+++ DB/add_partition.py
@@ -2,21 +2,76 @@
 
 # Author        : Ives
 # Date          : 2024-07-05
+
 import datetime
 import subprocess
 
+def manage_db_partitions(db_host, db_user, db_pwd, db_list, tables_list):
+    """
+    管理数据库分区的函数。
 
-# 每天定时执行即可
-db_list = [
-    'tenant_1001'
-]
+    参数:
+    db_host (str): 数据库主机地址
+    db_user (str): 数据库用户名
+    db_pwd (str): 数据库密码
+    db_list (list): 库列表
+    tables_list (list): 分区表列表
+    """
+    current_date = datetime.datetime.now()
 
-# DB连接信息
+    # 循环递增7天 (即新增7天后的7-14天分区, 删除30天前的30-37天分区)
+    for i in range(8):
+        next_week = current_date + datetime.timedelta(days=7 + i)
+        last_30_days = current_date - datetime.timedelta(days=30 - i)
+        target_time = datetime.time(1, 0, 0)
+
+        next_target_datetime = datetime.datetime.combine(next_week.date(), target_time)
+        last_target_datetime = datetime.datetime.combine(last_30_days.date(), target_time)
+
+        next_week_timestamp = int(next_target_datetime.timestamp() * 1000)
+        last_30_days_timestamp = int(last_target_datetime.timestamp() * 1000)
+
+        year_str_next = str(next_week.year)
+        month_str_next = str(next_week.month).zfill(2)
+        day_str_next = str(next_week.day).zfill(2)
+        date_str_next = "p" + year_str_next + month_str_next + day_str_next
+
+        year_str_last = str(last_30_days.year)
+        month_str_last = str(last_30_days.month).zfill(2)
+        day_str_last = str(last_30_days.day).zfill(2)
+        date_str_last = "p" + year_str_last + month_str_last + day_str_last
+
+        for dbs in db_list:
+            for tbs in tables_list:
+                check_drop_exists = f"SELECT 1 FROM information_schema.partitions WHERE table_schema = '{dbs}' AND table_name = '{tbs}' AND partition_name = '{date_str_last}'"
+                cmd_check_drop_exists = f"mysql -h{db_host} -u{db_user} -p{db_pwd} -e \"{check_drop_exists}\""
+                result_drop_exists = subprocess.run(cmd_check_drop_exists, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                if "1" in result_drop_exists.stdout.decode('utf-8'):
+                    sql_drop = f'use {dbs};ALTER TABLE {tbs} DROP PARTITION {date_str_last}'
+                    cmd_drop = f"mysql -h{db_host}  -u{db_user} -p{db_pwd} -e '{sql_drop}'"
+                    subprocess.run(cmd_drop, shell=True, check=True)
+                    print(f"Deleted partition {date_str_last} for table {tbs}")
+                else:
+                    print(f"Partition {date_str_last} does not exist for table {tbs}, skipping deletion")
+
+                check_add_exists = f"SELECT 1 FROM information_schema.partitions WHERE table_schema = '{dbs}' AND table_name = '{tbs}' AND partition_name = '{date_str_next}'"
+                cmd_check_add_exists = f"mysql -h{db_host} -u{db_user} -p{db_pwd} -e \"{check_add_exists}\""
+                result_add_exists = subprocess.run(cmd_check_add_exists, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                if "1" not in result_add_exists.stdout.decode('utf-8'):
+                    sql_add = f'use {dbs};ALTER TABLE {tbs} ADD PARTITION (partition {date_str_next} values less than ({next_week_timestamp}))'
+                    cmd_add = f"mysql -h{db_host} -u{db_user} -p{db_pwd} -e '{sql_add}'"
+                    subprocess.run(cmd_add, shell=True, check=True)
+                    print(f"Added partition {date_str_next} for table {tbs}")
+                else:
+                    print(f"Partition {date_str_next} already exists for table {tbs}, skipping addition")
+
+# 使用示例
 db_host = "ar0607.rwlb.singapore.rds.aliyuncs.com"
 db_user = "ives"
 db_pwd = "Cssl#123"
-
-# 分区表列表
+db_list = ['tenant_1001']
 tables_list = [
     'tab_financialchess',
     'tab_financialelectronic',
@@ -49,72 +104,4 @@ tables_list = [
     'tab_tenanttransfer'
 ]
 
-# 获取当前日期和时间
-current_date = datetime.datetime.now()
-
-
-# 循环递增7天 (即新增7天后的7-14天分区,删除30天前的30-37天分区)
-for i in range(8):
-
-
-    # 计算七天后的日期
-    next_week = current_date + datetime.timedelta(days=7+i)
-
-    # 计算30天前的日期
-    last_30_days = current_date - datetime.timedelta(days=30-i)
-
-    # 设置每天凌晨1点的时间
-    target_time = datetime.time(1, 0, 0)
-
-    # 组合日期和时间
-    next_target_datetime = datetime.datetime.combine(next_week.date(), target_time)
-    last_target_datetime = datetime.datetime.combine(last_30_days.date(), target_time)
-
-    # 七天后的时间戳
-    next_week_timestamp = int(next_target_datetime.timestamp() * 1000)
-
-    # 30天前的时间戳
-    last_30_days_timestamp = int(last_target_datetime.timestamp() * 1000)
-    
-    # 获取七天后的日期的年、月、日
-    year_str_next = str(next_week.year)
-    month_str_next = str(next_week.month).zfill(2)
-    day_str_next = str(next_week.day).zfill(2)
-
-    date_str_next = "p" + year_str_next + month_str_next + day_str_next
-
-    # 获取30天前的日期的年、月、日
-    year_str_last = str(last_30_days.year)
-    month_str_last = str(last_30_days.month).zfill(2)
-    day_str_last = str(last_30_days.day).zfill(2)
-
-    date_str_last = "p" + year_str_last + month_str_last + day_str_last
-    for dbs in db_list:
-        for tbs in tables_list:
-            # 检查是否存在要删除的分区
-            check_drop_exists = f"SELECT 1 FROM information_schema.partitions WHERE table_schema = '{dbs}' AND table_name = '{tbs}' AND partition_name = '{date_str_last}'"
-            cmd_check_drop_exists = f"mysql -h{db_host} -u{db_user} -p{db_pwd} -e \"{check_drop_exists}\""
-            result_drop_exists = subprocess.run(cmd_check_drop_exists, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            if "1" in result_drop_exists.stdout.decode('utf-8'):
-                # 删除30天前的分区
-                sql_drop = f'use {dbs};ALTER TABLE {tbs} DROP PARTITION {date_str_last}'
-                cmd_drop = f"mysql -h{db_host}  -u{db_user} -p{db_pwd} -e '{sql_drop}'"
-                subprocess.run(cmd_drop, shell=True, check=True)
-                print(f"Deleted partition {date_str_last} for table {tbs}")
-            else:
-                print(f"Partition {date_str_last} does not exist for table {tbs}, skipping deletion")
-
-            # 检查是否存在要添加的分区
-            check_add_exists = f"SELECT 1 FROM information_schema.partitions WHERE table_schema = '{dbs}' AND table_name = '{tbs}' AND partition_name = '{date_str_next}'"
-            cmd_check_add_exists = f"mysql -h{db_host} -u{db_user} -p{db_pwd} -e \"{check_add_exists}\""
-            result_add_exists = subprocess.run(cmd_check_add_exists, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            if "1" not in result_add_exists.stdout.decode('utf-8'):
-                # 添加七天后的分区
-                sql_add = f'use {dbs};ALTER TABLE {tbs} ADD PARTITION (partition {date_str_next} values less than ({next_week_timestamp}))'
-                cmd_add = f"mysql -h{db_host} -u{db_user} -p{db_pwd} -e '{sql_add}'"
-                subprocess.run(cmd_add, shell=True, check=True)
-                print(f"Added partition {date_str_next} for table {tbs}")
-            else:
-                print(f"Partition {date_str_next} already exists for table {tbs}, skipping addition")
+manage_db_partitions(db_host, db_user, db_pwd, db_list, tables_list)
